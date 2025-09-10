958bfd6542837f6bb26821d12b18fdf42598eed8
Prem Bijarane <165685254+prembijarane5112@users.noreply.github.com>
2025-09-11 00:02:05 +0530

    Update rectanglepattern.java

--- rectanglepattern.java
+++ rectanglepattern.java
@@ -63,20 +63,3 @@ System.out.print(i +" ");
 
 
   
-//  print as as this number instead of npattern
-  
-// this is startrangle pattern
-int n=4;
-  for(int i=1;i<=n;i++){
-  for(int j=1;j<=i;j++)
-    {
-System.out.print("*" +" ");
-    }
-}
-
-
-// output of this code
-* 
-*  *
-*  *  *
-*  *  *  *
