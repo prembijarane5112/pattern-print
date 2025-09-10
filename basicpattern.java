e564186a60988bd22aaa45d433703f9d6bfdfe98
Prem Bijarane <165685254+prembijarane5112@users.noreply.github.com>
2025-09-10 23:49:02 +0530

    Update basicpattern.java

--- basicpattern.java
+++ basicpattern.java
@@ -23,4 +23,9 @@ for(int i=0;i<=n;i++){
 //  print as as this number instead of npattern
 
 int n=4;
-  for(int i=1;i<=n;i++
+  for(int i=1;i<=n;i++){
+  for(int j=1;j<=i;j++)
+    {
+System.out.print("*" +" ");
+    }
+}
