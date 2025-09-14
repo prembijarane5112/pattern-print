69d146a075959988dcfeea4222c1876056ebd69a
Prem Bijarane <165685254+prembijarane5112@users.noreply.github.com>
2025-09-14 18:51:17 +0530

    Update tringlepattern.java

--- tringlepattern.java
+++ tringlepattern.java
@@ -36,6 +36,8 @@ public static void  main(String args[])
          for(int j=1;j<=i;j++)
            {
              System.out.print(j+" ");
+           }
+         
              
          
            
