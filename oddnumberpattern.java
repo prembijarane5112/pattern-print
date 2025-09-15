76f784a81fe4db31085222abb889194a251e480a
Prem Bijarane <165685254+prembijarane5112@users.noreply.github.com>
2025-09-15 14:39:15 +0530

    Update oddnumberpattern.java

--- oddnumberpattern.java
+++ oddnumberpattern.java
@@ -2,5 +2,9 @@ pubic class oddnumberpattern{
   public static void main(String args[])
   {
     int n=5;
-    for(int 
+    for(int i=1;i<=n;i++)
+      {
+for(int j=1;j<=i;j++)
+  {
+    System
     
